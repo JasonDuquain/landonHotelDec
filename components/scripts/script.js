48fec4370b707dd6c2fc8b10416b0fd04b245276
jasonduquain <jduquain@gmail.com>
2017-11-30 23:43:06 -0500

    final js styles complete - all done

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -59,5 +59,69 @@ window.addEventListener('resize', () => {
     
 });    
     
+/*****BACK TO TOP BUTTON*****/
+let docBody = document.body;
+let docElem = document.documentElement;
+let bttBtn = document.querySelector('#back-to-top');
+let heights = [docBody.scrollHeight, docBody.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight];
+let highestHeight = Math.max(...heights);
 
+
+window.addEventListener('scroll', () => {
+    let scrolltop = Math.max(docBody.scrollTop, docElem.scrollTop);
+    if (scrolltop >= (highestHeight / 3)) {
+        bttBtn.classList.add('visible');
+    } else {
+        bttBtn.classList.remove('visible');
+    }
+    
+});
+
+bttBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    docElem.scrollTop = 0;
+});
+
+/******STICKY HEADER*******/
+let nav = document.querySelector('#nav');
+let header = document.querySelector('#intro');
+
+function stick() {
+
+    if (header.getBoundingClientRect().bottom <= 0) {
+        nav.classList.add('fixed');
+        docBody.style.paddingTop = nav.getBoundingClientRect().height + 'px';
+    } else {
+        nav.classList.remove('fixed');
+        docBody.style.paddingTop = 0;
+    }
+    
+}
+
+window.addEventListener('scroll', stick);
+
+
+/******HIGHTLIGHT ON SCROLL******/
+/**The navs and sects do not equal each other as there is an initial 'welome sect so adjust the comparison accordingly ALSO LOOP THRU THE NAVS INSTEAD OF THE SECTS TO KEEP FROM GETTING THE 'type errs: cannot read propery getAttribute of undefined' in the console..still not sure why that fixes it? but it does ALSO IT LOOKS LIKE THIS WAS NVR NEEDED?:
+
+if ((sects[i+1]).getAttribute('id') === navs[i].getAttribute('href').slice(1)) ...
+	
+**/
+let sects = document.querySelectorAll('.scene:not(:only-of-type)'); //dont include footer
+let navs = document.querySelectorAll('a.icon')
+
+function highlight() {
+    navs.forEach((el, i) => {
+        
+            if (sects[i+1].getBoundingClientRect().top <=0 && sects[i+1].getBoundingClientRect().bottom >= 0) {
+                navs[i].classList.add('active');
+            } else {
+                navs[i].classList.remove('active');
+            }
+        
+    });
+}
+
+
+window.addEventListener('scroll', highlight);
 
